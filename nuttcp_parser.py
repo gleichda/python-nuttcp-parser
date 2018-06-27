bf47bc072ddfc606bbd52166ece8087112e8f1b5
David Gleich <dev.david@familie-gleich.de>
2018-06-27 13:17:29 +0200

    making usable for udp and tcp

--- nuttcp_parser.py
+++ nuttcp_parser.py
@@ -6,7 +6,12 @@ class Parser():
 
     def parse(self):
         lines = self.nuttcp_string.splitlines()
-        summary_split = lines[len(lines) - 1].split('=')
+        last_line = lines[len(lines) - 1]
+        if last_line.endswith('loss'):
+            protocol = 'udp'
+        else:
+            protocol = 'tcp'
+        summary_split = last_line.split('=')
         transmit = summary_split[0].split('/')
         transmitted_MB = float(transmit[0].split()[0]) # output of NUTTCP is always in MB
         duration = transmit[1].split()
@@ -14,21 +19,37 @@ class Parser():
         speed = float(rest[0])
         tx = int(rest[2])
         rx = int(rest[4])
-        retrans = int(rest[6])
-        rtt = float(rest[8])
-        result = NUTTCPResult()
+        if protocol == 'tcp':
+            retrans = int(rest[6])
+            rtt = float(rest[8])
+        else:
+            dropped = int(rest[6])
+            pakets = int(rest[8])
+            loss = float(rest[10])
+        result = NUTTCPResult(protocol)
         result.summary['transmitted_MB'] = transmitted_MB
         result.summary['duration'] = float(duration[0])
         result.summary['speed_Mbs'] = speed
         result.summary['RX'] = rx
         result.summary['TX'] = tx
-        result.summary['retrans'] = retrans
-        result.summary['RTT'] = rtt
+
+        #TCP only
+        if protocol == 'tcp':
+            result.summary['retrans'] = retrans
+            result.summary['RTT'] = rtt
+        else:
+            result.summary['dropped'] = dropped
+            result.summary['pakets'] = pakets
+            result.summary['loss'] = loss
         return result
 
 
 
 
 class NUTTCPResult():
-    def __init__(self):
-        self.summary = dict.fromkeys(['duration', 'transmitted_MB', 'speed_Mbs', 'RX', 'TX', 'retrans', 'RTT'])
+    def __init__(self, protocol):
+        self.protocol = protocol
+        if self.protocol == 'tcp':
+            self.summary = dict.fromkeys(['duration', 'transmitted_MB', 'speed_Mbs', 'RX', 'TX', 'retrans', 'RTT'])
+        else:
+            self.summary = dict.fromkeys(['duration', 'transmitted_MB', 'speed_Mbs', 'RX', 'TX', 'dropped', 'pakets', 'loss'])
